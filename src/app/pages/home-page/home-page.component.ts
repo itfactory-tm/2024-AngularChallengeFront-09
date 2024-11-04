03acb98be03f73ea5cafe0ecace551e9453a69d3
Devin Veraghtert <r0932546@student.thomasmore.be>
2024-11-04 13:53:34 +0100

    Fix layout home page + arrow down

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
-})
-export class HomePageComponent {
+})export class HomePageComponent {
 
+	// This will be used to reference the next section
+	@ViewChild('nextSection') nextSection!: ElementRef;
+
+	// This method will be called on button click
+	scrollToNextSection() {
+	  this.nextSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+	}
 }
