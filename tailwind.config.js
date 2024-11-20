bc65ddebd6d0e9e4c675ff43aa312e323ac87952
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2024-11-20 17:50:55 +0100

    dependencies: add prettier (formatting)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ...fontFamily,
+        impact: ['Impact Regular', ...fontFamily['sans']],
+      },
+      colors: {
+        primary: '#3F0A73',
+        secondary: '#EF35F2',
+        tertiary: '#601BA6',
+        text: '#601BA6',
+        accent: '#6BF2E5',
+      },
+    },
   },
   plugins: [],
-}
-
+};
