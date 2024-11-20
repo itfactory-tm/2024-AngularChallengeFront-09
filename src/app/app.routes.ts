7af1dec6710592055403422b670f52d7269b49dd
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2024-11-20 16:26:45 +0100

    admin: Generate admin page and add Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
 import { Routes } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 import { AuthGuard } from '@auth0/auth0-angular';
-
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LineUpComponent } from './pages/line-up/line-up.component';
 import { StagesComponent } from './pages/stages/stages.component';
 import { TicketsComponent } from './pages/tickets/tickets.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
-    {path:'', component:HomePageComponent},
-    {path:'line-up', component:LineUpComponent},
-    {path:'stages', component:StagesComponent},
-    {path:'tickets', component:TicketsComponent},
-    {path:'contact', component:ContactComponent},
-    {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  { path: '', component: HomePageComponent },
+  { path: 'line-up', component: LineUpComponent },
+  { path: 'stages', component: StagesComponent },
+  { path: 'tickets', component: TicketsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  //{path: 'admin', component: AdminComponent, canActivate: [AuthGuard]},
+  { path: 'admin', component: AdminComponent },
 ];
