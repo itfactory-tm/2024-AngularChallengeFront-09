df11736b6871fda31286c1a6834cac152a3a51cc
SanderJannes <r0938840@student.thomasmore.be>
2024-11-22 00:04:51 +0100

    all filters

--- src/app/pages/line-up/line-up.component.ts
+++ src/app/pages/line-up/line-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { groupBy, map, mergeMap, Observable, of, toArray } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ArtistComponent } from '../../components/artist/artist.component';
@@ -9,6 +9,10 @@ import { DayService } from '../../api/services/Day/day.service';
 import { FormatLineUpTitlePipe } from '../../pipes/format-line-up-title.pipe';
 import { GenreService } from '../../api/services/Genres/genre.service';
 import { DayResponseDto } from '../../api/dtos/Day/day-response-dto';
+import { PerformanceService } from '../../api/services/Performance/performance.service';
+import { GenreResponseDto } from '../../api/dtos/Genre/genre-response-dto';
+import { StageService } from '../../api/services/Stages/stage.service';
+import { StageDto } from '../../api/dtos/stage-dto';
 
 @Component({
   selector: 'app-line-up',
@@ -19,65 +23,89 @@ import { DayResponseDto } from '../../api/dtos/Day/day-response-dto';
 })
 export class  LineUpComponent implements OnInit {
   artistSchedule: Map<string, Observable<ArtistResponseDto[]>> = new Map();
-  activeFilter: string = "all";
+  activeFilter?: string = "all";
 
-  subfilters: Array<string> = [];
+  subfilters: string[] = [];
   activeSubFilterIndex: number = 0;
 
   constructor(private artistService : ArtistService, private dayService: DayService,
-              private genreService: GenreService) { }
+              private genreService: GenreService,
+              private stageService: StageService,
+              private performanceService: PerformanceService) { }
 
   ngOnInit(): void {
     let artists = this.artistService.getArtists();
     this.artistSchedule.set("All artists", artists);
   }
 
-  filterArtists(value: any): void {
-    this.activeFilter = value;
-    this.artistSchedule.clear();
-    
-    switch(value) {
-      case "all": {
-        this.subfilters = [];
+  updateSubFilter(n: number) {
+    this.activeSubFilterIndex = n;
+    this.updateFilterContent();
+  }
 
+  updateFilterContent(filterValue?: string) {
+    this.artistSchedule.clear();
+    this.subfilters = [];
+
+    if (filterValue) {
+      this.activeFilter = filterValue;
+      this.activeSubFilterIndex = 0;
+    }
+
+    switch (this.activeFilter) {
+      case "all": {
         let artists = this.artistService.getArtists();
         this.artistSchedule.set("All artists", artists);
         break;
       }
 
       case "byDay": {
-        this.activeSubFilterIndex = 0;
-        this.updateDayFilterContent();
+        this.dayService.getDays().subscribe(days => {
+          this.subfilters = days.map((day: DayResponseDto) => day.date);
+          
+          let day = days[this.activeSubFilterIndex]
+          let artists = this.artistService.getArtistsByDay(day.id);
+          this.artistSchedule.set(day.date, artists);
+        });
         break;
       }
 
       case "byGenre": {
-        this.subfilters = [];
+        this.genreService.getGenres().subscribe(genres => {
+          this.subfilters = genres.map((genre: GenreResponseDto) => genre.name);
 
-        this.genreService.getGenres().subscribe(genre => {
-          genre.forEach(genre => {
-            let artists = this.artistService.getArtistsByGenre(genre.id);
-            this.artistSchedule.set(genre.name, artists);
+          let genre = genres[this.activeSubFilterIndex];
+          let artists = this.artistService.getArtistsByGenre(genre.id);
+          this.artistSchedule.set(genre.name, artists);
+        });
+
+        break;
+      }
+
+      case "byStage": {
+        this.stageService.getStages().subscribe(stages => {
+          this.subfilters = stages.map((stage: StageDto) => stage.name);
+          let filter = this.subfilters[this.activeSubFilterIndex];
+
+          this.performanceService.getPerformances().pipe(
+          mergeMap((performances) => performances),
+          groupBy((performance) => performance.stage.name),
+          mergeMap((group$) =>
+            group$.pipe(
+              map((performance) => performance.artist),
+              toArray(),
+              map((artists) => ({ stageName: group$.key, artists }))
+            ))
+          )
+          .subscribe(({ stageName, artists }) => {
+            if (stageName === filter) {
+              this.artistSchedule.set(stageName, of(artists));
+            }
           });
         });
+
+        break;
       }
     }
   }
-
-  updateSubFilter(n: number) {
-    this.activeSubFilterIndex = n;
-    this.updateDayFilterContent();
-  }
-
-  updateDayFilterContent() {
-    this.artistSchedule.clear();
-
-    this.dayService.getDays().subscribe(days => {
-      this.subfilters = days.map((day: DayResponseDto) => day.date);
-      let day = days[this.activeSubFilterIndex]
-      let artists = this.artistService.getArtistsByDay(day.id);
-      this.artistSchedule.set(day.date, artists);
-    });
-  }
-
 }
