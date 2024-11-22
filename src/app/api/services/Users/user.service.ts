fabcd78a9b3ff70c3cdc5f8c88e8115cb82286c8
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2024-11-22 16:53:56 +0100

    user: sync user with auth0 to backend on login

---
+++ src/app/api/services/Users/user.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { UserResponseDto } from '../../dtos/User/user-response-dto';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  // TODO: Store base url for all api routes in .env file?
+  private baseUrl = 'https://localhost:7091';
+  constructor(
+    public auth: AuthService,
+    private http: HttpClient
+  ) {}
+
+  addUser(user: UserResponseDto): Observable<UserResponseDto> {
+    return this.http
+      .post<UserResponseDto>(this.baseUrl + '/api/Users', user)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Method to sync user with the backend
+  public syncUser(): void {
+    this.auth.user$.subscribe(user => {
+      if (user) {
+        const newUser: UserResponseDto = {
+          auth0Id: user.sub ?? '',
+          email: user.email ?? '',
+          isAdmin: false,
+        };
+
+        this.http
+          .post<UserResponseDto>(this.baseUrl + '/api/Users', newUser)
+          .pipe(catchError(this.handleError))
+          .subscribe({
+            next: response =>
+              console.log('User synced successfully:', response),
+            error: err => console.error('Error syncing user:', err),
+          });
+      }
+    });
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    const errorMessage =
+      error.status === 400
+        ? error.error
+        : 'Er is een onverwachte fout opgetreden.';
+    return throwError(() => new Error(errorMessage));
+  }
+}
