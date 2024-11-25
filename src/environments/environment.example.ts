b1646249d954ad6e521cb6672880767d25728e7e
Jan-Peter Dhallé <janpeter.dhalle@gmail.com>
2024-11-25 14:15:37 +0100

    security: manage auth0 credentials via angular environment files & (remove from github)

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  baseUrl: 'YOUR API URL (without endpoint)', // example: https://laserllama-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.azurewebsites.net/api
+  auth0Domain: 'YOUR AUTH0 DOMAIN',
+  auth0ClientId: 'YOUR AUTH0 CLIENT ID',
+};
