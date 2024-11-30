4bc833ed2be9963ddfa906a3325ba2c6c00ced83
SanderJannes <r0938840@student.thomasmore.be>
2024-11-30 15:04:07 +0100

    add one genre

--- src/app/components/artist-form/artist-form.component.ts
+++ src/app/components/artist-form/artist-form.component.ts
@@ -1,17 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ArtistRequestDto } from '../../api/dtos/Artist/artist-request-dto';
 import { ArtistService } from '../../api/services/Artist/artist.service';
 import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { GenreResponseDto } from '../../api/dtos/Genre/genre-response-dto';
+import { GenreService } from '../../api/services/Genres/genre.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { GenreRequestDto } from '../../api/dtos/Genre/genre-request-dto';
 
 @Component({
   selector: 'app-artist-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, AsyncPipe],
   templateUrl: './artist-form.component.html',
   styleUrl: './artist-form.component.css',
 })
-export class ArtistFormComponent {
-  constructor(private artistService: ArtistService) {}
+export class ArtistFormComponent implements OnInit {
+  constructor(private artistService: ArtistService, private genreService: GenreService) {}
+ 
   @Output() ErrorEvent = new EventEmitter<string>();
   @Input()
   edit = false;
@@ -22,18 +28,31 @@ export class ArtistFormComponent {
   @Input()
   selectedArtistDto: ArtistRequestDto = {
     name: '',
-    discogsId: '',
+    discogsId: ''
   };
 
+  selectedGenres: string[] = [];
+  genres$!: Observable<GenreResponseDto[]>;
+
+  ngOnInit(): void {
+    this.genres$ = this.genreService.getGenres();
+  }
+
   submitEdit() {
+    this.selectedArtistDto.genres = this.selectedGenres.map(name => ({ name }));
+
+    console.log(this.selectedArtistDto);
+
     this.artistService
       .updateArtist(this.selectedArtistId, this.selectedArtistDto)
       .subscribe(() => this.artistService.fetchArtists());
+
     this.cancelEdit();
   }
 
   cancelEdit() {
     this.edit = false;
+    this.selectedGenres = [];
     this.editChange.emit(this.edit);
     this.selectedArtistDto = {
       name: '',
@@ -42,6 +61,7 @@ export class ArtistFormComponent {
   }
 
   submitForm() {
+    this.selectedArtistDto.genres = this.selectedGenres.map(name => ({ name }));
     this.artistService.addArtist(this.selectedArtistDto).subscribe({
       next: () => {
         this.artistService.fetchArtists();
@@ -50,5 +70,6 @@ export class ArtistFormComponent {
         this.ErrorEvent.emit(err.message);
       },
     });
+    this.selectedGenres = [];
   }
 }
