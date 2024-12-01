164317b0f2b2ef1f53e5fcd3738f3d23f3d29d6f
Cissevos <r0938854@student.thomasmore.be>
2024-12-01 21:31:04 +0100

    vordering homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LoginButtonComponent } from "../../components/login/login-button";
+import { LogoutButtonComponent } from '../../components/login/logout-button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [LoginButtonComponent, LogoutButtonComponent, RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })export class HomePageComponent {
