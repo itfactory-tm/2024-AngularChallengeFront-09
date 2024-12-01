aade2bce9e50f014a4ebe7f9ee6c9ba5f565d1ca
Cissevos <r0938854@student.thomasmore.be>
2024-12-01 23:32:49 +0100

    Homepage afgewerkt

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { LoginButtonComponent } from "../../components/login/login-button";
-import { LogoutButtonComponent } from '../../components/login/logout-button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [LoginButtonComponent, LogoutButtonComponent, RouterLink],
+  imports: [RouterLink],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })export class HomePageComponent {
